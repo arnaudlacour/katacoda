315301de415c80b2a4154bf9b8bf2607244a05d6
arno <arnaudlacour@pingidentity.com>
2018-08-15 13:37:26 -0400

    This update: change the strategy to have curl output console war file with an absolute path to the tomcat autodeploy directory

--- ping-directory-basics/intro-code.sh
+++ ping-directory-basics/intro-code.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 docker pull pingidentity/ping-directory:katacoda &
 docker run -d -p 8080:8080 --name ping-console tomcat:alpine
-docker exec -it ping-console "cd /user/local/tomcat/webapps/ ; curl -k https://extensions.ping.directory/console.war -o console.war"
+docker exec -it ping-console curl -k https://extensions.ping.directory/console.war -o /user/local/tomcat/webapps/console.war
