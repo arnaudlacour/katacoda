70466dffc9863d66bac3343c60e1a599d09c3233
arno <arnaudlacour@pingidentity.com>
2018-08-15 10:22:29 -0400

    This update: tentative console deployment

--- ping-directory-basics/intro-course-data.sh
+++ ping-directory-basics/intro-course-data.sh
@@ -1,1 +1,4 @@
 docker pull pingidentity/ping-directory:katacoda
+docker pull tomcat:alpine
+docker run -d -p 8080:8080 --name ping-console tomcat:alpine
+docker cp console.war ping-console:/usr/local/tomcat/webapps/console.war
