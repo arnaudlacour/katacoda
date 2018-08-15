f2d53e86a076f2e3b408694a7763ac4bf1dac079
arno <arnaudlacour@pingidentity.com>
2018-08-15 15:06:24 -0400

    This update: yet another change in the strategy to deploy the console

--- ping-directory-basics/intro-code.sh
+++ ping-directory-basics/intro-code.sh
@@ -3,5 +3,6 @@ echo "Pulling the Ping Directory image"
 docker pull pingidentity/ping-directory:katacoda &
 
 echo "Starting Administration console container"
+docker pull tomcat:alpine
 docker run -d -p 8080:8080 --name ping-console tomcat:alpine
-sleep 30 && docker exec ping-console wget -q https://extensions.ping.directory/console.war -o /usr/local/tomcat/webapps/console.war
+sleep 5 && docker exec ping-console wget -q https://extensions.ping.directory/console.war -O /usr/local/tomcat/webapps/console.war
