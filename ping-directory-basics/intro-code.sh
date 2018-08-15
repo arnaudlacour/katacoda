32db1e05331be6fa4be47c4d32f31b6a490beafd
arno <arnaudlacour@pingidentity.com>
2018-08-15 16:24:13 -0400

    This update: fixed docker run command exposing wrong port

--- ping-directory-basics/intro-code.sh
+++ ping-directory-basics/intro-code.sh
@@ -4,6 +4,6 @@ docker pull pingidentity/ping-directory:katacoda &
 
 echo "Starting Administration console container"
 docker pull tomcat:alpine
-docker run -d -p 8443:8443 --name ping-console tomcat:alpine
+docker run -d --rm -p 8080:8080 --name ping-console tomcat:alpine
 sleep 5 && docker exec ping-console wget -q https://extensions.ping.directory/console.war -O /usr/local/tomcat/webapps/console.war
 docker cp index.html ping-console:/usr/local/tomcat/webapps/ROOT/index.html
