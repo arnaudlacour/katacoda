2cf49650022b422ac8f86ed8f969981aa5810359
arno <arnaudlacour@pingidentity.com>
2018-08-15 16:51:03 -0400

    This update: remove dashboard for now

--- ping-directory-basics/intro-code.sh
+++ ping-directory-basics/intro-code.sh
@@ -2,8 +2,8 @@
 echo "Pulling the Ping Directory image"
 docker pull pingidentity/ping-directory:katacoda &
 
-echo "Starting Administration console container"
-docker pull tomcat:alpine
-docker run -d --rm -p 8080:8080 --name ping-console tomcat:alpine
-sleep 5 && docker exec ping-console wget -q https://extensions.ping.directory/console.war -O /usr/local/tomcat/webapps/console.war
-docker cp index.html ping-console:/usr/local/tomcat/webapps/ROOT/index.html
+#echo "Starting Administration console container"
+#docker pull tomcat:alpine
+#docker run -d --rm -p 8080:8080 --name ping-console tomcat:alpine
+#sleep 5 && docker exec ping-console wget -q https://extensions.ping.directory/console.war -O /usr/local/tomcat/webapps/console.war
+#docker cp index.html ping-console:/usr/local/tomcat/webapps/ROOT/index.html
