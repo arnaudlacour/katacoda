e57c69a6ecdfdfbcc368b26cd7193618a571b5b9
arno <arnaudlacour@pingidentity.com>
2018-08-15 13:46:31 -0400

    This update: switch to wget as curl isn't on the tomcat image

--- ping-directory-basics/intro-code.sh
+++ ping-directory-basics/intro-code.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -x
 docker pull pingidentity/ping-directory:katacoda &
 docker run -d -p 8080:8080 --name ping-console tomcat:alpine
-docker exec -it ping-console curl -k https://extensions.ping.directory/console.war -o /user/local/tomcat/webapps/console.war
+docker exec ping-console wget -q https://extensions.ping.directory/console.war -o /usr/local/tomcat/webapps/console.war
