1074e7c2d16a1a3a99569b2b68a6f48ae5e00272
arno <arnaudlacour@pingidentity.com>
2018-08-15 14:43:44 -0400

    improve startup strategy

--- ping-directory-basics/intro-code.sh
+++ ping-directory-basics/intro-code.sh
@@ -1,4 +1,7 @@
-#!/bin/sh -x
+#!/bin/sh
+echo "Pulling the Ping Directory image"
 docker pull pingidentity/ping-directory:katacoda &
+
+echo "Starting Administration console container"
 docker run -d -p 8080:8080 --name ping-console tomcat:alpine
-docker exec ping-console wget -q https://extensions.ping.directory/console.war -o /usr/local/tomcat/webapps/console.war
+sleep 30 && docker exec ping-console wget -q https://extensions.ping.directory/console.war -o /usr/local/tomcat/webapps/console.war
