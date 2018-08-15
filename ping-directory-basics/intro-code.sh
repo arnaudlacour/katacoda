aeb26ceebecb68bd7a96e155309a1ac88c858d53
arno <arnaudlacour@pingidentity.com>
2018-08-15 13:35:49 -0400

    This update: change the strategy to foreground pull the image and start tomcat with the console

--- ping-directory-basics/intro-code.sh
+++ ping-directory-basics/intro-code.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -x
+docker pull pingidentity/ping-directory:katacoda &
 docker run -d -p 8080:8080 --name ping-console tomcat:alpine
-docker cp console.war ping-console:/usr/local/tomcat/webapps/console.war
+docker exec -it ping-console "cd /user/local/tomcat/webapps/ ; curl -k https://extensions.ping.directory/console.war -o console.war"
--- ping-directory-basics/intro-course-data.sh
+++ ping-directory-basics/intro-course-data.sh
@@ -1,5 +1,1 @@
 #!/bin/sh
-docker pull pingidentity/ping-directory:katacoda
-docker pull tomcat:alpine
-docker run -d -p 8080:8080 --name ping-console tomcat:alpine
-docker cp console.war ping-console:/usr/local/tomcat/webapps/console.war
