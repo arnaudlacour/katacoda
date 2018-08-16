3f8d012633a08c2ec0fb8baeb8f1c5abe49409e2
arno <arnaudlacour@pingidentity.com>
2018-08-16 13:25:40 -0400

    This update: hopeful this one fixes the background error

--- ping-directory-basics/intro-course-data.sh
+++ ping-directory-basics/intro-course-data.sh
@@ -1,1 +1,1 @@
-echo "+"
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Katacoda Ping Directory training starting!"}' https://hooks.slack.com/services/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
